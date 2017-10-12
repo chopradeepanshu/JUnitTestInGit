62f2a922e6219757cc378d9253be3c2a7115c3e1
chopradeepanshu <chopradeepanshu@gmail.com>
2017-10-12 07:54:47 +0530

    Added Calculator Test in my Test Class

---
+++ JUnitTestInGit/src/test/java/com/test/CalculatorTest.java
@@ -0,0 +1,33 @@
+package com.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void addtionOfTwoNumbers() throws Exception {
+		int a = 10;
+		int b = 10;
+		int c = a+b;
+		assertTrue(c==20);
+	}
+	
+	
+	@Test
+	public void differenceOfTwoNumbers() throws Exception {
+		int a = 10;
+		int b = 10;
+		int c = a-b;
+		assertTrue(c==0);
+	}
+
+	@Test
+	public void multiplyOfTwoNumbers() throws Exception {
+		int a = 10;
+		int b = 10;
+		int c = a*b;
+		assertTrue(c==100);
+	}
+}
--- JUnitTestInGit/src/test/java/com/test/SimpleTest.java
+++ JUnitTestInGit/src/test/java/com/test/SimpleTest.java
@@ -6,8 +6,6 @@ import org.junit.Test;
 
 public class SimpleTest {
 
-	
-	
 	@Test
 	public void aSimpleText() {
 		int a = 10;
